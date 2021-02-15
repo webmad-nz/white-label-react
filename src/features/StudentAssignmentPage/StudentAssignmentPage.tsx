4348215873bd87ef5ad9ab0109eb5df92ac397b7
Henry Seed <henry@amy.app>
2021-02-15 13:00:15 +1300

    adds sticky question and green bubble

--- src/features/StudentAssignmentPage/StudentAssignmentPage.tsx
+++ src/features/StudentAssignmentPage/StudentAssignmentPage.tsx
@@ -1,35 +1,33 @@
 import { getAmy, StudentAssignment } from "@amy-app/amy-app-js-sdk";
 import { Card, CardActionArea, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import { InstructionRender } from "../../components/amy-render/DefaultRenderer";
 import { useAmyReady } from "../../tools/amyHooks";
 
 const useStyles = makeStyles((theme) => ({
-    instruction: {
+    bubble: {
         borderstyle: "solid",
-        borderColor: theme.palette.info.main,
         borderLeftWidth: "8px",
         paddingLeft: "2px",
+    },
+    stickyBubble: {
+        position: "sticky",
+        top: 0,
+        zIndex: 100,
+    },
+    instruction: {
+        borderColor: theme.palette.info.main,
     },
     correctOption: {
-        borderstyle: "solid",
         borderColor: theme.palette.success.main,
-        borderLeftWidth: "8px",
-        paddingLeft: "2px",
     },
     incorrectOption: {
-        borderstyle: "solid",
         borderColor: theme.palette.error.main,
-        borderLeftWidth: "8px",
-        paddingLeft: "2px",
     },
     unknownOption: {
-        borderstyle: "solid",
         borderColor: theme.palette.secondary.main,
-        borderLeftWidth: "8px",
-        paddingLeft: "2px",
     },
 }));
 
@@ -38,7 +36,17 @@ export default function StudentAssignmentPage2() {
     const [exerciseId, setExerciseId] = useState("");
     const { studentAssignmentId } = useParams<{ studentAssignmentId: string }>();
     const [studentAssignment, setStudentAssignment] = useState<StudentAssignment.StudentAssignment>();
+    const [questionBubbleHeight, setQuestionBubbleheight] = useState(100);
+
     const classes = useStyles();
+
+    // Save the question height, lets us calculate how to make it stick to top of screen
+    const questionBubbleRef = useRef(null);
+    useEffect(() => {
+        if (questionBubbleRef.current) {
+            setQuestionBubbleheight(questionBubbleRef.current.clientHeight);
+        }
+    });
 
     useEffect(() => {
         if (studentAssignmentId && ready) {
@@ -73,13 +81,23 @@ export default function StudentAssignmentPage2() {
         return <>Done</>;
     }
 
-    const rows = [];
+    // find the most recent correct bubble so we can stick it to the top of the screen
+    const latestCorrect = [...exercise.rows]
+        .reverse()
+        .find((row) => row instanceof StudentAssignment.OptionRow && row.correct === "YES");
 
+    const rows = [];
     for (const row of exercise.rows) {
         if (row instanceof StudentAssignment.InstructionRow) {
             rows.push(
-                <Grid item xs={12} key={row.id}>
-                    <Card variant="outlined" className={classes.instruction}>
+                <Grid
+                    item
+                    xs={12}
+                    key={row.id}
+                    ref={questionBubbleRef}
+                    className={questionBubbleHeight < 200 ? classes.stickyBubble : ""}
+                >
+                    <Card variant="outlined" className={`${classes.bubble} ${classes.instruction}`}>
                         <InstructionRender text={row.text} />
                     </Card>
                 </Grid>,
@@ -89,7 +107,7 @@ export default function StudentAssignmentPage2() {
         if (row instanceof StudentAssignment.FeedbackRow) {
             rows.push(
                 <Grid item xs={12} key={row.id}>
-                    <Card variant="outlined" className={classes.instruction}>
+                    <Card variant="outlined" className={`${classes.bubble} ${classes.instruction}`}>
                         <InstructionRender text={row.text} />
                     </Card>
                 </Grid>,
@@ -101,7 +119,7 @@ export default function StudentAssignmentPage2() {
                 for (const option of row.options) {
                     rows.push(
                         <Grid item xs={12} key={`${row.id}_${option.id}`}>
-                            <Card variant="outlined" className={classes.unknownOption}>
+                            <Card variant="outlined" className={`${classes.bubble} ${classes.unknownOption}`}>
                                 <CardActionArea
                                     onClick={() => {
                                         option.select();
@@ -117,10 +135,24 @@ export default function StudentAssignmentPage2() {
                 const o = row.options.find((e) => e.selected);
                 if (o) {
                     rows.push(
-                        <Grid item xs={12} key={`selected_${row.id}_${o.id}`}>
+                        <Grid
+                            item
+                            xs={12}
+                            key={`selected_${row.id}_${o.id}`}
+                            className={row === latestCorrect ? classes.stickyBubble : ""}
+                            style={
+                                row === latestCorrect
+                                    ? {
+                                          top: questionBubbleHeight < 200 ? `${questionBubbleHeight}px` : 0,
+                                      }
+                                    : {}
+                            }
+                        >
                             <Card
                                 variant="outlined"
-                                className={row.correct === "YES" ? classes.correctOption : classes.incorrectOption}
+                                className={`${classes.bubble} ${
+                                    row.correct === "YES" ? classes.correctOption : classes.incorrectOption
+                                }`}
                             >
                                 <InstructionRender text={o.text} />
                             </Card>
@@ -135,7 +167,7 @@ export default function StudentAssignmentPage2() {
     if (exercise.finished) {
         rows.push(
             <Grid item xs={12} key={`finished_${exercise.id}`}>
-                <Card variant="outlined" className={classes.unknownOption}>
+                <Card variant="outlined" className={`${classes.bubble} ${classes.unknownOption}`}>
                     <CardActionArea
                         onClick={() => {
                             // select next exercise
